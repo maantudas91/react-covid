48f7a83aee86d567f752c5b11d63f9000b33b8ce
maantudas91 <maantudas91@gmail.com>
2020-03-26 00:54:25 +0530

    1 file modified

--- State.js
+++ State.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const State = ( { name, cords, fillColor }) =>{
   return (
-    <path fill={fillColor} d={cords} pointerEvents="all" style={{cursor: 'pointer'}}>
+    <path fill={fillColor} d={cords} pointerEvents="all" style={{cursor: 'pointer'}} onClick={() => console.log("working")}>
       <title>19.77% from {name}</title>
     </path>
   )
