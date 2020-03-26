a55d56f0c61fbd1a8f14b4d9f1e3666dcf62e182
maantudas91 <maantudas91@gmail.com>
2020-03-26 23:54:55 +0530

    1 file modified

--- Map.js
+++ Map.js
@@ -6,32 +6,27 @@ import data from './map-data.json';
 import Sidemap from "./Sidemap";
 
 const Map = () =>{
-
    const mapList = data.map((state, index) =>{
-   console.log(state)
     return (
-         <State key={index}
-      fillColor="#ffffff" 
-      cords={state.cords} 
-      name={state.name}>
-    </State>
+      <State 
+        key={index}
+        fillColor="#ffffff" 
+        cords={state.cords} 
+        name={state.name}>
+      </State>
     )
   });
 
   return (
-      <div className="svg-parent">
+    <div className="svg-parent">
       <svg id="chart" width="650" height="750" viewBox="0 0 650 750" preserveAspectRatio="xMidYMid meet">
         <Sidemap/ >
-        
         <g className="states">
           {mapList}
         </g>
-        
         <India></India>
-  </svg>
-  {mapList}
-</div>
-
+      </svg>
+    </div>
   )
 }
 
